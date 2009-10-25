f7b98d199043a0e921450825124cbfa15060a050
aishwar <aishwar@208d1520-c027-11de-8cd0-f941f5a20a08>
2009-10-25 20:40:10 +0000

    Modified the code to fix the 2 bugs - where reclicking on the background capture after initiating the differencing threw an exception and reclicking on the begin differencing got successively slowers. Done mainly through 1 instance of capture object and named function called for begin frame difference event handling (the named function is unregisted each time before it is registered - to avoid duplicate addition)

--- EmguCVTest/EmguCVTest/Form1.cs
+++ EmguCVTest/EmguCVTest/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 using Emgu.CV;
 using Emgu.CV.UI;
 using Emgu.CV.CvEnum;
@@ -16,57 +17,79 @@ namespace EmguCVTest
     public partial class Form1 : Form
     {
 
-        Image<Gray, Byte> backgroundImage;
-        const String DEFAULT_TEXT_BTN_BG_CAPTURE = "Take Background Image [5s]";
-        const String DEFAULT_TEXT_BTN_BEGIN_DIFFERENCING = "Begin Differencing";
+        Image<Gray, Byte> _backgroundImage;
+        // One instance of the capture resource
+        Capture _capture;
+        // One instance of the difference viewer
+        ImageViewer _differenceViewer;
 
         public Form1()
         {
             InitializeComponent();
-            this.backgroundImage = new Image<Gray, byte>(640, 480);
+            // Initialize the components
+            _backgroundImage = new Image<Gray, byte>(640, 480);
+            _differenceViewer = new ImageViewer();
+            InitializeCamera();
+        }
+
+        // This is the function that updates the difference frames and performs
+        // the thresholding on the difference
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            Image<Gray, Byte> frame = _capture.QueryGrayFrame();
+            Image<Gray, Byte> difference = new Image<Gray, byte>(640, 480);
+            CvInvoke.cvAbsDiff(_backgroundImage, frame, difference);
+            Image<Gray, Byte> thresholded = new Image<Gray,byte>(640, 480);
+            thresholded = difference.ThresholdBinary(new Gray(20), new Gray(255));
+            _differenceViewer.Image = thresholded;
         }
 
         private void btnBgCapture_Click(object sender, EventArgs e)
         {
+            // Initialize in case it was destroyed somewhere else
+            InitializeCamera();
             MessageBox.Show("Background capture will happen 5s after you click OK");
-
-            using (Capture capture = new Capture())
+            // Warm up the camera - let it take 100 frames, and finish its auto-adjustment
+            for (int i = 0; i < 100; i++)
             {
-                capture.FlipHorizontal = true;
-                for (int i = 0; i < 50; i++)
-                {
-                    capture.QueryGrayFrame();
-                    System.Threading.Thread.Sleep(100);
-                }
-                this.backgroundImage = capture.QueryGrayFrame();
+                Thread.Sleep(50);
+                _capture.QueryGrayFrame();
+            }
+            // Actual background capture
+            _backgroundImage = _capture.QueryGrayFrame();
+            MessageBox.Show("Background capture complete");
+        }
+
+        // Initializes the camera resource, if it does not currently exist
+        private void InitializeCamera()
+        {
+            if (_capture == null)
+            {
+                _capture = new Capture();
+                _capture.FlipHorizontal = true;
             }
         }
 
         private void btnBeginDifferencing_Click(object sender, EventArgs e)
         {
-            ImageViewer viewer = new ImageViewer(); // create an image viewer
-
-            Capture capture = new Capture(); // create a camera capture
-            capture.FlipHorizontal = true;
-            Image<Gray, Byte> frame = new Image<Gray, byte>(640,480);
-            Application.Idle += new EventHandler(delegate(object newsender, EventArgs newe)
-            {   // run this until the application is closed
-                System.Threading.Thread.Sleep(50);
-                frame = capture.QueryGrayFrame();
-                Image<Gray, Byte> difference = new Image<Gray, byte>(640, 480);
-                CvInvoke.cvAbsDiff(this.backgroundImage, frame, difference);
-                viewer.Image = difference.ThresholdBinary(new Gray(15), new Gray(255));
-                //viewer.Image = difference;
-            });
+            // Initialize the camera - in case it was destroyed somewhere else
+            InitializeCamera();
+            if (_capture != null)
+            {
+                // Unregister the event handler *in case* it was already registed (reclick of this button)
+                Application.Idle -= new EventHandler(ProcessFrame);
+                // Register the event handler
+                Application.Idle += new EventHandler(ProcessFrame);
+            }
             
-            viewer.ShowDialog();    // show the image viewer
+            _differenceViewer.ShowDialog();    // show the image viewer
         }
 
         private void btnShowBackground_Click(object sender, EventArgs e)
         {
             ImageViewer viewer = new ImageViewer();
             // Show the background image
-            viewer.Image = backgroundImage;
+            viewer.Image = _backgroundImage;
             viewer.ShowDialog();
         }
     }
