ad6dabbebe562e4d5bf0546b6a2a9c7141a9bfd2
jesse.rj <jesse.rj@208d1520-c027-11de-8cd0-f941f5a20a08>
2009-10-28 19:31:37 +0000

    Added a captureImageBox and moved _differenceImage to the main form, the two can now be seen side by side

--- EmguCVTest/EmguCVTest/Form1.Designer.cs
+++ EmguCVTest/EmguCVTest/Form1.Designer.cs
@@ -28,9 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnBgCapture = new System.Windows.Forms.Button();
             this.btnBeginDifferencing = new System.Windows.Forms.Button();
             this.btnShowBackground = new System.Windows.Forms.Button();
+            this.capturedImageBox = new Emgu.CV.UI.ImageBox();
+            this.motionImageBox = new Emgu.CV.UI.ImageBox();
+            this.backgroundImage = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.capturedImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.motionImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundImage)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBgCapture
@@ -63,16 +70,51 @@
             this.btnShowBackground.UseVisualStyleBackColor = true;
             this.btnShowBackground.Click += new System.EventHandler(this.btnShowBackground_Click);
             // 
+            // capturedImageBox
+            // 
+            this.capturedImageBox.BackColor = System.Drawing.SystemColors.ControlText;
+            this.capturedImageBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.capturedImageBox.Location = new System.Drawing.Point(228, 12);
+            this.capturedImageBox.Name = "capturedImageBox";
+            this.capturedImageBox.Size = new System.Drawing.Size(409, 353);
+            this.capturedImageBox.TabIndex = 3;
+            this.capturedImageBox.TabStop = false;
+            // 
+            // motionImageBox
+            // 
+            this.motionImageBox.BackColor = System.Drawing.SystemColors.ControlText;
+            this.motionImageBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.motionImageBox.Location = new System.Drawing.Point(643, 12);
+            this.motionImageBox.Name = "motionImageBox";
+            this.motionImageBox.Size = new System.Drawing.Size(397, 353);
+            this.motionImageBox.TabIndex = 4;
+            this.motionImageBox.TabStop = false;
+            // 
+            // backgroundImage
+            // 
+            this.backgroundImage.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.backgroundImage.Location = new System.Drawing.Point(45, 186);
+            this.backgroundImage.Name = "backgroundImage";
+            this.backgroundImage.Size = new System.Drawing.Size(177, 173);
+            this.backgroundImage.TabIndex = 5;
+            this.backgroundImage.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(1048, 371);
+            this.Controls.Add(this.backgroundImage);
+            this.Controls.Add(this.motionImageBox);
+            this.Controls.Add(this.capturedImageBox);
             this.Controls.Add(this.btnShowBackground);
             this.Controls.Add(this.btnBeginDifferencing);
             this.Controls.Add(this.btnBgCapture);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.capturedImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.motionImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundImage)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -82,6 +124,9 @@
         private System.Windows.Forms.Button btnBgCapture;
         private System.Windows.Forms.Button btnBeginDifferencing;
         private System.Windows.Forms.Button btnShowBackground;
+        private Emgu.CV.UI.ImageBox capturedImageBox;
+        private Emgu.CV.UI.ImageBox motionImageBox;
+        private Emgu.CV.UI.ImageBox backgroundImage;
     }
 }
 
