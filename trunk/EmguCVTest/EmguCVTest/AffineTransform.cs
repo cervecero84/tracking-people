7e2e29c37b54b19d49847d8cdacf4fe0cef90117
jesse.rj <jesse.rj@208d1520-c027-11de-8cd0-f941f5a20a08>
2009-11-21 06:40:57 +0000

    Changed from Affine to perspective transformation will update remaining differences to change text from affine to perspective
    still need to input in order of bottom left, top left, top right, bottom right

--- trunk/EmguCVTest/EmguCVTest/AffineTransform.cs
+++ trunk/EmguCVTest/EmguCVTest/AffineTransform.cs
@@ -16,8 +16,10 @@ namespace EmguCVTest
 {
     public partial class AffineTransform : Form
     {
-        PointF[] srcpt = new PointF[3];
-        PointF[] dstpt = new PointF[3];
+        PointF[] srcpt = new PointF[4];
+        PointF[] dstpt = new PointF[4];
+
+        enum Position { BL, TL, TR, BR };
 
         int ptCounter = 0;
         
@@ -27,7 +29,7 @@ namespace EmguCVTest
         int _orgFrameHeight = 480;
         Capture _capture;
 
-        IntPtr warpMat = CvInvoke.cvCreateMat(2, 3, MAT_DEPTH.CV_64F);
+        IntPtr warpMat = CvInvoke.cvCreateMat(3, 3, MAT_DEPTH.CV_64F);
         
 
         Image<Bgr, Byte> imageTransform;
@@ -39,16 +41,19 @@ namespace EmguCVTest
             _capture = c;
 
             imageTransform = _capture.QueryFrame();
+            imageTransform = imageTransform.Resize(_frameWidth, _frameHeight);
             imageBackup = imageTransform;
 
-            imgImageBox.Image = imageTransform.Resize(_frameWidth, _frameHeight);
+            imgImageBox.Image = imageTransform;//.Resize(_frameWidth, _frameHeight);
 
-            dstpt[0].X = 0;
-            dstpt[0].Y = 299;
-            dstpt[1].X = 0;
-            dstpt[1].Y = 0;
-            dstpt[2].X = 399;
-            dstpt[2].Y = 0;
+            dstpt[(int)Position.BL].X = 0;
+            dstpt[(int)Position.BL].Y = 399;
+            dstpt[(int)Position.TL].X = 0;
+            dstpt[(int)Position.TL].Y = 0;
+            dstpt[(int)Position.TR].X = 299;
+            dstpt[(int)Position.TR].Y = 0;
+            dstpt[(int)Position.BR].X = 299;
+            dstpt[(int)Position.BR].Y = 399;
 
         }
 
@@ -58,9 +63,10 @@ namespace EmguCVTest
 
 
             imageTransform = _capture.QueryFrame();
+            imageTransform = imageTransform.Resize(_frameWidth, _frameHeight);
             imageBackup = imageTransform;
 
-            imgImageBox.Image = imageTransform.Resize(_frameWidth,_frameHeight);
+            imgImageBox.Image = imageTransform;//.Resize(_frameWidth,_frameHeight);
 
 
             //Cvinvoke.cvGetAffineTransform(srcTri, dstTri, warp_mat);
@@ -73,19 +79,19 @@ namespace EmguCVTest
 
         private void imgImageBox_MouseClick(object sender, MouseEventArgs e)
         {
-            if (ptCounter == 3)
+            if (ptCounter == 4)
             {
                 ptCounter = 0;
                 imageTransform = imageBackup;
             }
-            if (ptCounter < 3)
+            if (ptCounter < 4)
             {
-                srcpt[ptCounter].X = e.X * _orgFrameWidth/_frameWidth;
-                srcpt[ptCounter].Y = e.Y * _orgFrameHeight/_frameHeight;
+                srcpt[ptCounter].X = e.X; //* _orgFrameWidth/_frameWidth;
+                srcpt[ptCounter].Y = e.Y; //* _orgFrameHeight/_frameHeight;
 
                 Cross2DF scrCrossTest = new Cross2DF(srcpt[ptCounter], 5, 5);
                 imageTransform.Draw(scrCrossTest, new Bgr(Color.Red), 2);
-                imgImageBox.Image = imageTransform.Resize(_frameWidth, _frameHeight);
+                imgImageBox.Image = imageTransform;//.Resize(_frameWidth, _frameHeight);
 
                 ptCounter += 1;
             }
@@ -99,26 +105,54 @@ namespace EmguCVTest
             Cross2DF scrCross1 = new Cross2DF(srcpt[0], 5, 5);
             Cross2DF scrCross2 = new Cross2DF(srcpt[1], 5, 5);
             Cross2DF scrCross3 = new Cross2DF(srcpt[2], 5, 5);
+            Cross2DF scrCross4 = new Cross2DF(srcpt[3], 5, 5);
 
             imageTransform.Draw(scrCross1, new Bgr(Color.Red), 2);
             imageTransform.Draw(scrCross2, new Bgr(Color.Red), 2);
             imageTransform.Draw(scrCross3, new Bgr(Color.Red), 2);
+            imageTransform.Draw(scrCross4, new Bgr(Color.Red), 2);
 
-            imgImageBox.Image = imageTransform.Resize(_frameWidth, _frameHeight);
+            imgImageBox.Image = imageTransform;//.Resize(_frameWidth, _frameHeight);
 
 
         }
 
         private void btnTransform_Click(object sender, EventArgs e)
         {
-            PointF[] srcTri = new PointF[] { srcpt[0], srcpt[1], srcpt[2] };
-            PointF[] dstTri = new PointF[] { dstpt[0], dstpt[1], dstpt[2] };
+            PointF[] srcTri = new PointF[] { srcpt[0], srcpt[1], srcpt[2], srcpt[3] };
 
-            CvInvoke.cvGetAffineTransform(srcTri, dstTri, warpMat);
+            dstpt[(int)Position.BL].X = srcpt[0].X;
+            dstpt[(int)Position.BL].Y = srcpt[0].Y;
+            dstpt[(int)Position.TL].X = srcpt[0].X;
+            dstpt[(int)Position.TL].Y = srcpt[1].Y;
+            dstpt[(int)Position.TR].X = srcpt[2].X;
+            dstpt[(int)Position.TR].Y = srcpt[1].Y;
+            dstpt[(int)Position.BR].X = srcpt[2].X;
+            dstpt[(int)Position.BR].Y = srcpt[0].Y;
+
+
+            Cross2DF dstCross1 = new Cross2DF(dstpt[(int)Position.BL], 5, 5);
+            Cross2DF dstCross2 = new Cross2DF(dstpt[(int)Position.TL], 5, 5);
+            Cross2DF dstCross3 = new Cross2DF(dstpt[(int)Position.TR], 5, 5);
+            Cross2DF dstCross4 = new Cross2DF(dstpt[(int)Position.BR], 5, 5);
+
+            imageTransform.Draw(dstCross1, new Bgr(Color.Orange), 2);
+            imageTransform.Draw(dstCross2, new Bgr(Color.Orange), 2);
+            imageTransform.Draw(dstCross3, new Bgr(Color.Orange), 2);
+            imageTransform.Draw(dstCross4, new Bgr(Color.Orange), 2);
+
+            imgImageBox.Image = imageTransform;//.Resize(_frameWidth, _frameHeight);
+
+
+            PointF[] dstTri = dstpt;//new PointF[] { dstpt[Position.BL], dstpt[Position.TL], dstpt[Position.TR], dstpt[Position.BL] };
+
+            //CvInvoke.cvGetAffineTransform(srcTri, dstTri, warpMat);
+            CvInvoke.cvGetPerspectiveTransform(srcTri, dstTri, warpMat);
 
             MCvScalar fillvar = new MCvScalar(0);
             imageBoxPers.Image = new Image<Bgr, byte>(_frameWidth, _frameHeight, new Bgr(0, 0, 0));
-            CvInvoke.cvWarpAffine(imgImageBox.Image.Ptr, imageBoxPers.Image.Ptr, warpMat, 0, fillvar);
+            //CvInvoke.cvWarpAffine(imgImageBox.Image.Ptr, imageBoxPers.Image.Ptr, warpMat, 0, fillvar);
+            CvInvoke.cvWarpPerspective(imgImageBox.Image.Ptr, imageBoxPers.Image.Ptr, warpMat, 0, fillvar);
         }
     }
 }
