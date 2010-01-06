3ac0a5e2bbe871876364f71729c28f6e5b23c5a6
aishwar <aishwar@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-01-06 04:43:09 +0000

    Testing out branching in SVN; checking in the part I am working on build off the IdeaTester project. NOTE: This is not being checked in, in the main IdeaTester project. This is the thing I am building off of it - a branch.
    Not sure if this is going to work...

---
+++ branches/CamShiftTracking/Main.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace IdeaTester
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnVideo = new System.Windows.Forms.Button();
+            this.ofdSourceVideo = new System.Windows.Forms.OpenFileDialog();
+            this.lblPlayingFrom = new System.Windows.Forms.Label();
+            this.lblVideoSource = new System.Windows.Forms.Label();
+            this.btnCamera = new System.Windows.Forms.Button();
+            this.ibxSource = new Emgu.CV.UI.ImageBox();
+            this.ibxOutput = new Emgu.CV.UI.ImageBox();
+            this.cbxVideo = new System.Windows.Forms.CheckBox();
+            this.txtFrameLimit = new System.Windows.Forms.TextBox();
+            this.btnSetFrameLimit = new System.Windows.Forms.Button();
+            this.txtKernelSize = new System.Windows.Forms.TextBox();
+            this.txtColorSigma = new System.Windows.Forms.TextBox();
+            this.txtSpaceSigma = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.cbxFilterType = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ibxSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibxOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(184, 366);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(156, 32);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Capture + Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnVideo
+            // 
+            this.btnVideo.Location = new System.Drawing.Point(782, 318);
+            this.btnVideo.Name = "btnVideo";
+            this.btnVideo.Size = new System.Drawing.Size(75, 32);
+            this.btnVideo.TabIndex = 4;
+            this.btnVideo.Text = "Video (...)";
+            this.btnVideo.UseVisualStyleBackColor = true;
+            this.btnVideo.Click += new System.EventHandler(this.btnVideo_Click);
+            // 
+            // ofdSourceVideo
+            // 
+            this.ofdSourceVideo.FileName = "openFileDialog1";
+            // 
+            // lblPlayingFrom
+            // 
+            this.lblPlayingFrom.AutoSize = true;
+            this.lblPlayingFrom.Location = new System.Drawing.Point(535, 328);
+            this.lblPlayingFrom.Name = "lblPlayingFrom";
+            this.lblPlayingFrom.Size = new System.Drawing.Size(70, 13);
+            this.lblPlayingFrom.TabIndex = 5;
+            this.lblPlayingFrom.Text = "Playing from: ";
+            // 
+            // lblVideoSource
+            // 
+            this.lblVideoSource.AutoSize = true;
+            this.lblVideoSource.Location = new System.Drawing.Point(611, 328);
+            this.lblVideoSource.Name = "lblVideoSource";
+            this.lblVideoSource.Size = new System.Drawing.Size(43, 13);
+            this.lblVideoSource.TabIndex = 6;
+            this.lblVideoSource.Text = "Camera";
+            // 
+            // btnCamera
+            // 
+            this.btnCamera.Location = new System.Drawing.Point(701, 318);
+            this.btnCamera.Name = "btnCamera";
+            this.btnCamera.Size = new System.Drawing.Size(75, 32);
+            this.btnCamera.TabIndex = 7;
+            this.btnCamera.Text = "Camera";
+            this.btnCamera.UseVisualStyleBackColor = true;
+            this.btnCamera.Click += new System.EventHandler(this.btnCamera_Click);
+            // 
+            // ibxSource
+            // 
+            this.ibxSource.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ibxSource.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ibxSource.Location = new System.Drawing.Point(21, 12);
+            this.ibxSource.Name = "ibxSource";
+            this.ibxSource.Size = new System.Drawing.Size(400, 300);
+            this.ibxSource.TabIndex = 8;
+            this.ibxSource.TabStop = false;
+            // 
+            // ibxOutput
+            // 
+            this.ibxOutput.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ibxOutput.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ibxOutput.Location = new System.Drawing.Point(457, 12);
+            this.ibxOutput.Name = "ibxOutput";
+            this.ibxOutput.Size = new System.Drawing.Size(400, 300);
+            this.ibxOutput.TabIndex = 9;
+            this.ibxOutput.TabStop = false;
+            // 
+            // cbxVideo
+            // 
+            this.cbxVideo.AutoSize = true;
+            this.cbxVideo.Location = new System.Drawing.Point(21, 327);
+            this.cbxVideo.Name = "cbxVideo";
+            this.cbxVideo.Size = new System.Drawing.Size(119, 17);
+            this.cbxVideo.TabIndex = 10;
+            this.cbxVideo.Text = "Continuous Capture";
+            this.cbxVideo.UseVisualStyleBackColor = true;
+            this.cbxVideo.CheckedChanged += new System.EventHandler(this.cbxVideo_CheckedChanged);
+            // 
+            // txtFrameLimit
+            // 
+            this.txtFrameLimit.Location = new System.Drawing.Point(146, 325);
+            this.txtFrameLimit.Name = "txtFrameLimit";
+            this.txtFrameLimit.Size = new System.Drawing.Size(32, 20);
+            this.txtFrameLimit.TabIndex = 11;
+            // 
+            // btnSetFrameLimit
+            // 
+            this.btnSetFrameLimit.Location = new System.Drawing.Point(184, 318);
+            this.btnSetFrameLimit.Name = "btnSetFrameLimit";
+            this.btnSetFrameLimit.Size = new System.Drawing.Size(75, 32);
+            this.btnSetFrameLimit.TabIndex = 12;
+            this.btnSetFrameLimit.Text = "Set F. Limit";
+            this.btnSetFrameLimit.UseVisualStyleBackColor = true;
+            this.btnSetFrameLimit.Click += new System.EventHandler(this.btnSetFrameLimit_Click);
+            // 
+            // txtKernelSize
+            // 
+            this.txtKernelSize.Location = new System.Drawing.Point(23, 373);
+            this.txtKernelSize.Name = "txtKernelSize";
+            this.txtKernelSize.Size = new System.Drawing.Size(45, 20);
+            this.txtKernelSize.TabIndex = 13;
+            // 
+            // txtColorSigma
+            // 
+            this.txtColorSigma.Location = new System.Drawing.Point(74, 373);
+            this.txtColorSigma.Name = "txtColorSigma";
+            this.txtColorSigma.Size = new System.Drawing.Size(45, 20);
+            this.txtColorSigma.TabIndex = 14;
+            // 
+            // txtSpaceSigma
+            // 
+            this.txtSpaceSigma.Location = new System.Drawing.Point(125, 373);
+            this.txtSpaceSigma.Name = "txtSpaceSigma";
+            this.txtSpaceSigma.Size = new System.Drawing.Size(45, 20);
+            this.txtSpaceSigma.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "kernel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "color";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "space";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(457, 366);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(126, 13);
+            this.lblStatus.TabIndex = 19;
+            this.lblStatus.Text = "Status messages go here";
+            // 
+            // cbxFilterType
+            // 
+            this.cbxFilterType.FormattingEnabled = true;
+            this.cbxFilterType.Items.AddRange(new object[] {
+            "Blur",
+            "Gaussian",
+            "Median",
+            "Bilateral"});
+            this.cbxFilterType.Location = new System.Drawing.Point(266, 328);
+            this.cbxFilterType.Name = "cbxFilterType";
+            this.cbxFilterType.Size = new System.Drawing.Size(74, 21);
+            this.cbxFilterType.TabIndex = 20;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 405);
+            this.Controls.Add(this.cbxFilterType);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSpaceSigma);
+            this.Controls.Add(this.txtColorSigma);
+            this.Controls.Add(this.txtKernelSize);
+            this.Controls.Add(this.btnSetFrameLimit);
+            this.Controls.Add(this.txtFrameLimit);
+            this.Controls.Add(this.cbxVideo);
+            this.Controls.Add(this.ibxOutput);
+            this.Controls.Add(this.ibxSource);
+            this.Controls.Add(this.btnCamera);
+            this.Controls.Add(this.lblVideoSource);
+            this.Controls.Add(this.lblPlayingFrom);
+            this.Controls.Add(this.btnVideo);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Main";
+            this.Text = "Idea Tester";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ibxSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibxOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnVideo;
+        private System.Windows.Forms.OpenFileDialog ofdSourceVideo;
+        private System.Windows.Forms.Label lblPlayingFrom;
+        private System.Windows.Forms.Label lblVideoSource;
+        private System.Windows.Forms.Button btnCamera;
+        private Emgu.CV.UI.ImageBox ibxSource;
+        private Emgu.CV.UI.ImageBox ibxOutput;
+        private System.Windows.Forms.CheckBox cbxVideo;
+        private System.Windows.Forms.TextBox txtFrameLimit;
+        private System.Windows.Forms.Button btnSetFrameLimit;
+        private System.Windows.Forms.TextBox txtKernelSize;
+        private System.Windows.Forms.TextBox txtColorSigma;
+        private System.Windows.Forms.TextBox txtSpaceSigma;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ComboBox cbxFilterType;
+    }
+}
+
---
+++ branches/CamShiftTracking/Main.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+namespace IdeaTester
+{
+    public partial class Main : Form
+    {
+
+        Capture _camera;
+        int _msPerFrame = (int)(1000.0/30);
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // Activate the camera
+            btnCamera_Click(sender, e);
+        }
+
+        private void btnVideo_Click(object sender, EventArgs e)
+        {
+            String videoSourceFileName = "";
+            if (ofdSourceVideo.ShowDialog() == DialogResult.OK)
+            {
+                videoSourceFileName = ofdSourceVideo.FileName;
+                Capture tmp = _camera;
+                try
+                {
+                    tmp = new Capture(videoSourceFileName);
+                }
+                catch (NullReferenceException)
+                {
+                    // This exception happens when the file that was tried to be opened
+                    // was not a readable video file
+                    MessageBox.Show("Unreadable video file. No action done.");
+                }
+                _camera = tmp;
+                lblVideoSource.Text = "File";
+            }
+        }
+
+        private void btnCamera_Click(object sender, EventArgs e)
+        {
+            _camera = new Capture(0);
+            lblVideoSource.Text = "Camera";
+        }
+
+        private void btnStartCapture_Click(object sender, EventArgs e)
+        {
+            ibxSource.Image = _camera.QueryFrame().Resize(400, 300, INTER.CV_INTER_CUBIC);
+        }
+
+        private void cbxVideo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbxVideo.Checked)
+            {
+                Application.Idle += new EventHandler(processFrame);
+            }
+            else
+            {
+                Application.Idle -= new EventHandler(processFrame);
+            }
+        }
+
+        private void processFrame(object sender, EventArgs e)
+        {
+            DateTime curr = DateTime.Now;
+            performOperation();
+            TimeSpan currTime = DateTime.Now - curr;
+            Thread.Sleep( (_msPerFrame > currTime.Milliseconds) ? _msPerFrame - currTime.Milliseconds : 0);
+        }
+
+        /// <summary>
+        /// This is the ONLY function you would require to modify, you could add other helper functions,
+        /// but the other existing functions can stay the way they are
+        /// 
+        /// All the image processing goes in here
+        /// </summary>
+        private void performOperation()
+        {
+            Image<Bgr, Byte> source = _camera.QueryFrame();
+            
+            // Reached the last frame in the source - happens in video files
+            if (source == null)
+            {
+                cbxVideo.Checked = false;
+                _camera = new Capture(ofdSourceVideo.FileName);
+                source = _camera.QueryFrame();
+                if (source == null)
+                {
+                    lblStatus.Text = "Video source no longer available";
+                    return;
+                }
+            }
+
+            source = source.Resize(400, 300, INTER.CV_INTER_CUBIC);
+            Image<Ycc, Byte> bandTemplate = new Image<Ycc, byte>("template/OrangeBand.jpg");
+
+            int kSize;
+            if (!Int32.TryParse(txtKernelSize.Text.ToString(), out kSize)) kSize = 3;
+            int cSigma;
+            if (!Int32.TryParse(txtColorSigma.Text.ToString(), out cSigma)) cSigma = 15;
+            int sSigma;
+            if (!Int32.TryParse(txtSpaceSigma.Text.ToString(), out sSigma)) sSigma = 5;
+            txtKernelSize.Text = kSize.ToString();
+            txtColorSigma.Text = cSigma.ToString();
+            txtSpaceSigma.Text = sSigma.ToString();
+
+            Image<Bgr, Byte> result;
+            if (cbxFilterType.SelectedText == "Blur")
+            {
+                 result = source.SmoothBlur(kSize, cSigma);
+            }
+            else if (cbxFilterType.SelectedText == "Gaussian")
+            {
+                result = source.SmoothGaussian(kSize);
+            }
+            else if (cbxFilterType.SelectedText == "Median")
+            {
+                result = source.SmoothMedian(kSize);
+            }
+            else
+            {
+                result = source.SmoothBilatral(kSize, cSigma, sSigma);
+            }
+            lblStatus.Text = cbxFilterType.SelectedText + " smoothing being applied: (" + kSize.ToString() + ", " + cSigma.ToString() + ", " + sSigma.ToString() + ")";
+
+            ibxSource.Image = source;
+            ibxOutput.Image = result;
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            processFrame(sender, e);
+        }
+
+        private void btnSetFrameLimit_Click(object sender, EventArgs e)
+        {
+            _msPerFrame = (int)(1000.0/Int32.Parse(txtFrameLimit.Text));
+        }
+    }
+}
