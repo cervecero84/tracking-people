60d8b738b25cacab6519ff303a56512b9ee8b44e
jesse.rj <jesse.rj@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-01-12 00:12:53 +0000

    Converts real table top coordinates to points in the image

--- trunk/EmguCVTest/EmguCVTest/AffineTransform.cs
+++ trunk/EmguCVTest/EmguCVTest/AffineTransform.cs
@@ -23,6 +23,7 @@ namespace EmguCVTest
         PointF[] leftSide = new PointF[1];
         PointF[] leftSideT = new PointF[1];
         PointF[] rightSide = new PointF[1];
+        PointF[] rightSideT = new PointF[1];
         //PointF[] rightSideTransform = new PointF[0];
 
         enum Position { BL, TL, TR, BR };
@@ -34,6 +35,9 @@ namespace EmguCVTest
         int _orgFrameWidth = 640;
         int _orgFrameHeight = 480;
         Capture _capture;
+
+        int tableTopHeight = 67;
+        int tableTopWidth = 108;
 
         Matrix<double> warpMat = new Matrix<double>(3, 3);// CvInvoke.cvCreateMat(3, 3, MAT_DEPTH.CV_64F);
         Matrix<double> invWarpMat = new Matrix<double>(3, 3); //CvInvoke.cvCreateMat(3, 3, MAT_DEPTH.CV_64F);
@@ -54,6 +58,29 @@ namespace EmguCVTest
         {
             InitializeComponent();
             _capture = c;
+
+
+
+
+            this.subscription1.BeginInit();
+            this.subscription1.Pattern = "/coordinates";
+
+            // this next line creates an event handler that will fire when a notification is received
+            this.subscription1.Notified += new SubscriptionEventHandler(subscription1_Notified);
+
+            this.subscription1.EndInit();
+
+            this.subscription1.BeginInit();
+            this.subscription1.Pattern = "/coordinates/pts";
+
+            // this next line creates an event handler that will fire when a notification is received
+            this.subscription1.Notified += new SubscriptionEventHandler(subscription1_Notified);
+
+            this.subscription1.EndInit();
+
+            this.sd.Url = "tcp://localhost:shareD";
+            this.sd.Open();
+
 
             imageTransform = _capture.QueryFrame();
             imageTransform = imageTransform.Resize(_frameWidth, _frameHeight);
@@ -104,8 +131,11 @@ namespace EmguCVTest
                     srcpt[ptCounter].X = e.X; //* _orgFrameWidth/_frameWidth;
                     srcpt[ptCounter].Y = e.Y; //* _orgFrameHeight/_frameHeight;
 
-                    sd["/coordinates/pts"] = new SharedDictionary.Vector();
-                    sd["/coordinates/pts#0"] = new Point(e.X, e.Y);
+                    //sd["/coordinates/pts"] = new SharedDictionary.Vector();
+                    //sd["/coordinates/pts#0"] = new Point(e.X, e.Y);
+                    ////this.sd["/coordinate/pts"] = new SharedDictionary.Vector();
+                    ////this.sd["/coordinate/pts#0"] = e.X.ToString();
+                    this.sd["/value"] = 5;
 
                     Cross2DF scrCrossTest = new Cross2DF(srcpt[ptCounter], 5, 5);
                     imageTransform.Draw(scrCrossTest, new Bgr(Color.Red), 2);
@@ -116,28 +146,35 @@ namespace EmguCVTest
             }
             else if (lblCurrentPoint.Text == "Coordinate Transform -->")
             {
-                lblCurrentPoint.Text = "Draw Point";
+                //lblCurrentPoint.Text = "Draw Point";
                 //lblCurrentPoint.Text = "Click on left side";
                 //currentCoordinates = Guid.NewGuid();
-                sd["/coordinates/pts"] = new SharedDictionary.Vector();
-                sd["/coordinates/pts#0"] = new Point ( e.X, e.Y );
+                ////sd["/coordinates/pts"] = new SharedDictionary.Vector();
+                ////sd["/coordinates/pts#0"] = new Point ( e.X, e.Y );
                 //sd["/coordinates#-0"] = currentCoordinates;
 
                 leftSide[0].X = e.X; //* _orgFrameWidth/_frameWidth;
                 leftSide[0].Y = e.Y; //* _orgFrameHeight/_frameHeight;
+
+                CvArray<Byte> arr;
+                Matrix<double> m = new Matrix<double>(1, 1, 2);
+                Matrix<double> n = new Matrix<double>(1, 1, 2);
                 
-                double[] data = { leftSide[0].X, leftSide[0].Y, 1};
-                Matrix<double> leftSidePtr = new Matrix<double>(data);
-                Matrix<double> leftSideTransformed = new Matrix<double>(3, 1);
+                double[] data = { leftSide[0].X, leftSide[0].Y};
+                m.Data[0, 0] = e.X;
+                m.Data[0, 1] = e.Y;
+
+                //Matrix<double> leftSidePtr = new Matrix<double>(data).Transpose();
+                //Matrix<double> leftSideTransformed = new Matrix<double>(1, 3);
                 //Matrix<double> leftSidePtrT = new Matrix<double>();
                 //Matrix<double> wMat = new Matrix<double>(warpMat);
                 //CvInvoke.cvTranspose(leftSidePtr, leftSidePtrT);
                 //CvInvoke.cvTranspose(leftSideTransformed, leftSideTransformed);
-                CvInvoke.cvPerspectiveTransform(leftSidePtr.Ptr, leftSideTransformed.Ptr, warpMat.Ptr);
+                CvInvoke.cvPerspectiveTransform(m, n, warpMat.Ptr);
                 
 
-                leftSideT[0].X = (float)leftSideTransformed[1, 1];
-                leftSideT[0].Y = (float)leftSideTransformed[1, 2];
+                leftSideT[0].X = (float)n.Data[0,0];
+                leftSideT[0].Y = (float)n.Data[0, 1];
 
                 Cross2DF scrCrossTest2 = new Cross2DF(leftSideT[0], 5, 5);
                 imagePerspective.Draw(scrCrossTest2, new Bgr(Color.Green), 2);
@@ -147,6 +184,8 @@ namespace EmguCVTest
                 Cross2DF scrCrossTest1 = new Cross2DF(leftSide[0], 5, 5);
                 imageTransform.Draw(scrCrossTest1, new Bgr(Color.Green), 2);
                 imgImageBox.Image = imageTransform;//.Resize(_frameWidth, _frameHeight);
+
+                lblTouchPt.Text = "[" + leftSideT[0].X + " , " + leftSideT[0].Y + "]";
 
             }
 
@@ -158,12 +197,29 @@ namespace EmguCVTest
            
             if (lblCurrentPoint.Text == "Coordinate Transform <--")
             {
-                lblCurrentPoint.Text = "Draw Point";
+                //lblCurrentPoint.Text = "Draw Point";
                 //lblCurrentPoint.Text = "Click on left side";
                 rightSide[0].X = e.X; //* _orgFrameWidth/_frameWidth;
                 rightSide[0].Y = e.Y; //* _orgFrameHeight/_frameHeight;
                 //CvInvoke.cvSetData(leftSide, e.X e.Y 1, //leftSide = [e.X, e.Y, 1];
 
+                CvArray<Byte> arr;
+                Matrix<double> m = new Matrix<double>(1, 1, 2);
+                Matrix<double> n = new Matrix<double>(1, 1, 2);
+
+                double[] data = { rightSide[0].X, rightSide[0].Y };
+                m.Data[0, 0] = e.X;
+                m.Data[0, 1] = e.Y;
+
+                CvInvoke.cvPerspectiveTransform(m, n, invWarpMat.Ptr);
+
+
+                rightSideT[0].X = (float)n.Data[0, 0];
+                rightSideT[0].Y = (float)n.Data[0, 1];
+
+                Cross2DF scrCrossTest2 = new Cross2DF(rightSideT[0], 5, 5);
+                imageTransform.Draw(scrCrossTest2, new Bgr(Color.Green), 2);
+                imgImageBox.Image = imageTransform;//.Resize(_frameWidth, _frameHeight);
 
 
                 Cross2DF scrCrossTest1 = new Cross2DF(rightSide[0], 5, 5);
@@ -202,6 +258,13 @@ namespace EmguCVTest
             dstpt[(int)Position.BR].X = srcpt[2].X;
             dstpt[(int)Position.BR].Y = srcpt[0].Y;
 
+            lblBL.Text = "[" + (int)dstpt[(int)Position.BL].X + " , " + (int)dstpt[(int)Position.BL].Y + "]";
+            lblTL.Text = "[" + (int)dstpt[(int)Position.TL].X + " , " + (int)dstpt[(int)Position.TL].Y + "]";
+            lblTR.Text = "[" + (int)dstpt[(int)Position.TR].X + " , " + (int)dstpt[(int)Position.TR].Y + "]";
+            lblBR.Text = "[" + (int)dstpt[(int)Position.BR].X + " , " + (int)dstpt[(int)Position.BR].Y + "]";
+
+
+
 
             Cross2DF dstCross1 = new Cross2DF(dstpt[(int)Position.BL], 5, 5);
             Cross2DF dstCross2 = new Cross2DF(dstpt[(int)Position.TL], 5, 5);
@@ -235,21 +298,61 @@ namespace EmguCVTest
 
         private void subscription1_Notified(object sender, SubscriptionEventArgs e)
         {
-            if (e.Reason == SubscriptionNotification.Add)
-            {
-
-            }
+            int x = (int)e.Entry.Value;
+            lblCurrentPoint.Text = x.ToString();
         }
 
         private void AffineTransform_Load(object sender, EventArgs e)
         {
-            sd["/coordinates"] = new SharedDictionary.Vector();
-            sd.Open();
+            //this.sd.Url = "tcp://localhost:shareD";
+            //this.sd.Open();
+            this.sd["/coordinates"] = 6;
+
+            //sd["/coordinates"] = new SharedDictionary.Vector();
+            //sd.Open();
         }
 
         private void sd_Opened(object sender, EventArgs e)
         {
             this.Refresh();
+        }
+
+        private void AffineTransform_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.sd.Close();
+        }
+
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _capture.Dispose();
+                _capture = new Capture(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnDrawPt_Click(object sender, EventArgs e)
+        {
+            int PtX;
+            int PtY;
+            Int32.TryParse(txtPtX.Text, out PtX);
+            Int32.TryParse(txtPtY.Text, out PtY);
+
+            //lblTouchPt.Text = (PtY / tableTopHeight) * ((int)dstpt[(int)Position.BL].Y - (int)dstpt[(int)Position.TL].Y) + (int)dstpt[(int)Position.BL].Y;
+            //lblTouchPt.Text = (PtX / tableTopHeight) * ((int)dstpt[(int)Position.BL].X - (int)dstpt[(int)Position.TL].X) + (int)dstpt[(int)Position.BL].X;
+
+            float percentFromLeft = (float) PtX / tableTopWidth;
+            float percentFromBottom = (float) PtY / tableTopHeight;
+
+            PointF pointToDraw = new PointF();
+            pointToDraw.X = (dstpt[(int)Position.BR].X - dstpt[(int)Position.BL].X) * percentFromLeft + dstpt[(int)Position.BL].X;
+            pointToDraw.Y = (dstpt[(int)Position.TL].Y - dstpt[(int)Position.BL].Y) * percentFromBottom + dstpt[(int)Position.BL].Y;
+
+            Cross2DF crossToDraw = new Cross2DF(pointToDraw, 5, 5);
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(imageBoxPers.Image.Bitmap);
+            img.Draw(crossToDraw, new Bgr(Color.Purple), 3);
+            imageBoxPers.Image = img;
+
         }
     }
 }
