a7c61d8b8d3fcf9dc82a98a0a743f2938e1ed6d3
jesse.rj <jesse.rj@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-01-12 00:12:53 +0000

    Converts real table top coordinates to points in the image

--- EmguCVTest/EmguCVTest/AffineTransform.Designer.cs
+++ EmguCVTest/EmguCVTest/AffineTransform.Designer.cs
@@ -39,6 +39,16 @@
             this.sd = new GroupLab.Networking.SharedDictionary(this.components);
             this.subscription1 = new GroupLab.Networking.Subscription(this.components);
             this.subscription2 = new GroupLab.Networking.Subscription(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenImage = new System.Windows.Forms.Button();
+            this.lblBL = new System.Windows.Forms.Label();
+            this.lblTL = new System.Windows.Forms.Label();
+            this.lblTR = new System.Windows.Forms.Label();
+            this.lblBR = new System.Windows.Forms.Label();
+            this.lblTouchPt = new System.Windows.Forms.Label();
+            this.txtPtX = new System.Windows.Forms.TextBox();
+            this.txtPtY = new System.Windows.Forms.TextBox();
+            this.btnDrawPt = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imgImageBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBoxPers)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.subscription1)).BeginInit();
@@ -59,6 +69,7 @@
             // 
             this.imgImageBox.BackColor = System.Drawing.SystemColors.ControlText;
             this.imgImageBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.imgImageBox.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
             this.imgImageBox.Location = new System.Drawing.Point(12, 41);
             this.imgImageBox.Name = "imgImageBox";
             this.imgImageBox.Size = new System.Drawing.Size(400, 300);
@@ -70,6 +81,7 @@
             // 
             this.imageBoxPers.BackColor = System.Drawing.SystemColors.ControlText;
             this.imageBoxPers.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.imageBoxPers.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
             this.imageBoxPers.Location = new System.Drawing.Point(418, 41);
             this.imageBoxPers.Name = "imageBoxPers";
             this.imageBoxPers.Size = new System.Drawing.Size(400, 300);
@@ -99,9 +111,9 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(742, 13);
+            this.btnClear.Location = new System.Drawing.Point(790, 13);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.Size = new System.Drawing.Size(26, 23);
             this.btnClear.TabIndex = 11;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -109,7 +121,7 @@
             // lblCurrentPoint
             // 
             this.lblCurrentPoint.AutoSize = true;
-            this.lblCurrentPoint.Location = new System.Drawing.Point(418, 12);
+            this.lblCurrentPoint.Location = new System.Drawing.Point(353, 13);
             this.lblCurrentPoint.Name = "lblCurrentPoint";
             this.lblCurrentPoint.Size = new System.Drawing.Size(59, 13);
             this.lblCurrentPoint.TabIndex = 12;
@@ -132,11 +144,103 @@
             this.subscription2.Dictionary = this.sd;
             this.subscription2.Pattern = "/coordinates";
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnOpenImage
+            // 
+            this.btnOpenImage.Location = new System.Drawing.Point(256, 13);
+            this.btnOpenImage.Name = "btnOpenImage";
+            this.btnOpenImage.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenImage.TabIndex = 13;
+            this.btnOpenImage.Text = "Open Image";
+            this.btnOpenImage.UseVisualStyleBackColor = true;
+            this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
+            // 
+            // lblBL
+            // 
+            this.lblBL.AutoSize = true;
+            this.lblBL.Location = new System.Drawing.Point(498, 22);
+            this.lblBL.Name = "lblBL";
+            this.lblBL.Size = new System.Drawing.Size(20, 13);
+            this.lblBL.TabIndex = 14;
+            this.lblBL.Text = "BL";
+            // 
+            // lblTL
+            // 
+            this.lblTL.AutoSize = true;
+            this.lblTL.Location = new System.Drawing.Point(498, 6);
+            this.lblTL.Name = "lblTL";
+            this.lblTL.Size = new System.Drawing.Size(20, 13);
+            this.lblTL.TabIndex = 15;
+            this.lblTL.Text = "TL";
+            // 
+            // lblTR
+            // 
+            this.lblTR.AutoSize = true;
+            this.lblTR.Location = new System.Drawing.Point(564, 6);
+            this.lblTR.Name = "lblTR";
+            this.lblTR.Size = new System.Drawing.Size(22, 13);
+            this.lblTR.TabIndex = 16;
+            this.lblTR.Text = "TR";
+            // 
+            // lblBR
+            // 
+            this.lblBR.AutoSize = true;
+            this.lblBR.Location = new System.Drawing.Point(564, 22);
+            this.lblBR.Name = "lblBR";
+            this.lblBR.Size = new System.Drawing.Size(22, 13);
+            this.lblBR.TabIndex = 17;
+            this.lblBR.Text = "BR";
+            // 
+            // lblTouchPt
+            // 
+            this.lblTouchPt.AutoSize = true;
+            this.lblTouchPt.Location = new System.Drawing.Point(626, 6);
+            this.lblTouchPt.Name = "lblTouchPt";
+            this.lblTouchPt.Size = new System.Drawing.Size(51, 13);
+            this.lblTouchPt.TabIndex = 18;
+            this.lblTouchPt.Text = "Touch Pt";
+            // 
+            // txtPtX
+            // 
+            this.txtPtX.Location = new System.Drawing.Point(629, 19);
+            this.txtPtX.Name = "txtPtX";
+            this.txtPtX.Size = new System.Drawing.Size(26, 20);
+            this.txtPtX.TabIndex = 19;
+            // 
+            // txtPtY
+            // 
+            this.txtPtY.Location = new System.Drawing.Point(661, 19);
+            this.txtPtY.Name = "txtPtY";
+            this.txtPtY.Size = new System.Drawing.Size(26, 20);
+            this.txtPtY.TabIndex = 20;
+            // 
+            // btnDrawPt
+            // 
+            this.btnDrawPt.Location = new System.Drawing.Point(693, 16);
+            this.btnDrawPt.Name = "btnDrawPt";
+            this.btnDrawPt.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawPt.TabIndex = 21;
+            this.btnDrawPt.Text = "Draw Pt";
+            this.btnDrawPt.UseVisualStyleBackColor = true;
+            this.btnDrawPt.Click += new System.EventHandler(this.btnDrawPt_Click);
+            // 
             // AffineTransform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(828, 348);
+            this.Controls.Add(this.btnDrawPt);
+            this.Controls.Add(this.txtPtY);
+            this.Controls.Add(this.txtPtX);
+            this.Controls.Add(this.lblTouchPt);
+            this.Controls.Add(this.lblBR);
+            this.Controls.Add(this.lblTR);
+            this.Controls.Add(this.lblTL);
+            this.Controls.Add(this.lblBL);
+            this.Controls.Add(this.btnOpenImage);
             this.Controls.Add(this.lblCurrentPoint);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnTransform);
@@ -147,6 +251,7 @@
             this.Name = "AffineTransform";
             this.Text = "AffineTransform";
             this.Load += new System.EventHandler(this.AffineTransform_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AffineTransform_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.imgImageBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBoxPers)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.subscription1)).EndInit();
@@ -168,5 +273,15 @@
         private GroupLab.Networking.SharedDictionary sd;
         private GroupLab.Networking.Subscription subscription1;
         private GroupLab.Networking.Subscription subscription2;
+        private System.Windows.Forms.Button btnOpenImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblBL;
+        private System.Windows.Forms.Label lblTouchPt;
+        private System.Windows.Forms.Label lblBR;
+        private System.Windows.Forms.Label lblTR;
+        private System.Windows.Forms.Label lblTL;
+        private System.Windows.Forms.TextBox txtPtY;
+        private System.Windows.Forms.TextBox txtPtX;
+        private System.Windows.Forms.Button btnDrawPt;
     }
 }
