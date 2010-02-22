2c1f84b525a1ba07d6f7abb9d4dff408eec2d024
aishwar <aishwar@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-02-22 07:20:18 +0000

    Warper.cs: Warping code from Johny Lee, warp function overloaded for ease of use Main.cs:
    IR and WebCam Calibration - to common coordinate space
    IR points detected by Wiimote OR clicks in the Wiimote viewer - get transformed
    to the common/normalized coordinate space. Then this value is reverse warped into
    the web camera viewer's space and the point is drawn.
    Each sensor (IR 1-4 have different colors and are drawn by their colors)

--- IdeaTester/Main.Designer.cs
+++ IdeaTester/Main.Designer.cs
@@ -38,8 +38,13 @@
             this.ibxSource = new Emgu.CV.UI.ImageBox();
             this.ibxOutput = new Emgu.CV.UI.ImageBox();
             this.cbxVideo = new System.Windows.Forms.CheckBox();
-            this.txtFrameLimit = new System.Windows.Forms.TextBox();
-            this.btnSetFrameLimit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnWiimoteCalibrate = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.cbxFlipHorizontal = new System.Windows.Forms.CheckBox();
+            this.btnCameraCalibrate = new System.Windows.Forms.Button();
+            this.lblFPS = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ibxSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ibxOutput)).BeginInit();
             this.SuspendLayout();
@@ -100,21 +105,25 @@
             // 
             this.ibxSource.BackColor = System.Drawing.SystemColors.ControlText;
             this.ibxSource.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ibxSource.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
             this.ibxSource.Location = new System.Drawing.Point(21, 12);
             this.ibxSource.Name = "ibxSource";
             this.ibxSource.Size = new System.Drawing.Size(400, 300);
             this.ibxSource.TabIndex = 8;
             this.ibxSource.TabStop = false;
+            this.ibxSource.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ibxSource_MouseClick);
             // 
             // ibxOutput
             // 
             this.ibxOutput.BackColor = System.Drawing.SystemColors.ControlText;
             this.ibxOutput.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ibxOutput.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
             this.ibxOutput.Location = new System.Drawing.Point(457, 12);
             this.ibxOutput.Name = "ibxOutput";
             this.ibxOutput.Size = new System.Drawing.Size(400, 300);
             this.ibxOutput.TabIndex = 9;
             this.ibxOutput.TabStop = false;
+            this.ibxOutput.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ibxOutput_MouseClick);
             // 
             // cbxVideo
             // 
@@ -127,30 +136,85 @@
             this.cbxVideo.UseVisualStyleBackColor = true;
             this.cbxVideo.CheckedChanged += new System.EventHandler(this.cbxVideo_CheckedChanged);
             // 
-            // txtFrameLimit
+            // label1
             // 
-            this.txtFrameLimit.Location = new System.Drawing.Point(308, 325);
-            this.txtFrameLimit.Name = "txtFrameLimit";
-            this.txtFrameLimit.Size = new System.Drawing.Size(32, 20);
-            this.txtFrameLimit.TabIndex = 11;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Camera";
             // 
-            // btnSetFrameLimit
+            // label2
             // 
-            this.btnSetFrameLimit.Location = new System.Drawing.Point(346, 318);
-            this.btnSetFrameLimit.Name = "btnSetFrameLimit";
-            this.btnSetFrameLimit.Size = new System.Drawing.Size(75, 32);
-            this.btnSetFrameLimit.TabIndex = 12;
-            this.btnSetFrameLimit.Text = "Set F. Limit";
-            this.btnSetFrameLimit.UseVisualStyleBackColor = true;
-            this.btnSetFrameLimit.Click += new System.EventHandler(this.btnSetFrameLimit_Click);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(454, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Wiimote";
+            // 
+            // btnWiimoteCalibrate
+            // 
+            this.btnWiimoteCalibrate.Location = new System.Drawing.Point(21, 356);
+            this.btnWiimoteCalibrate.Name = "btnWiimoteCalibrate";
+            this.btnWiimoteCalibrate.Size = new System.Drawing.Size(156, 32);
+            this.btnWiimoteCalibrate.TabIndex = 15;
+            this.btnWiimoteCalibrate.Text = "Wiimote IR Calibrate";
+            this.btnWiimoteCalibrate.UseVisualStyleBackColor = true;
+            this.btnWiimoteCalibrate.Click += new System.EventHandler(this.btnWiimoteCalibrate_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(464, 356);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 16;
+            this.lblStatus.Text = "Status";
+            // 
+            // cbxFlipHorizontal
+            // 
+            this.cbxFlipHorizontal.AutoSize = true;
+            this.cbxFlipHorizontal.Location = new System.Drawing.Point(183, 350);
+            this.cbxFlipHorizontal.Name = "cbxFlipHorizontal";
+            this.cbxFlipHorizontal.Size = new System.Drawing.Size(92, 17);
+            this.cbxFlipHorizontal.TabIndex = 17;
+            this.cbxFlipHorizontal.Text = "Flip Horizontal";
+            this.cbxFlipHorizontal.UseVisualStyleBackColor = true;
+            this.cbxFlipHorizontal.CheckedChanged += new System.EventHandler(this.cbxFlipHorizontal_CheckedChanged);
+            // 
+            // btnCameraCalibrate
+            // 
+            this.btnCameraCalibrate.Location = new System.Drawing.Point(21, 394);
+            this.btnCameraCalibrate.Name = "btnCameraCalibrate";
+            this.btnCameraCalibrate.Size = new System.Drawing.Size(156, 32);
+            this.btnCameraCalibrate.TabIndex = 18;
+            this.btnCameraCalibrate.Text = "Camera Calibrate";
+            this.btnCameraCalibrate.UseVisualStyleBackColor = true;
+            this.btnCameraCalibrate.Click += new System.EventHandler(this.btnCameraCalibrate_Click);
+            // 
+            // lblFPS
+            // 
+            this.lblFPS.AutoSize = true;
+            this.lblFPS.Location = new System.Drawing.Point(308, 328);
+            this.lblFPS.Name = "lblFPS";
+            this.lblFPS.Size = new System.Drawing.Size(27, 13);
+            this.lblFPS.TabIndex = 19;
+            this.lblFPS.Text = "FPS";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 362);
-            this.Controls.Add(this.btnSetFrameLimit);
-            this.Controls.Add(this.txtFrameLimit);
+            this.ClientSize = new System.Drawing.Size(884, 435);
+            this.Controls.Add(this.lblFPS);
+            this.Controls.Add(this.btnCameraCalibrate);
+            this.Controls.Add(this.cbxFlipHorizontal);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnWiimoteCalibrate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cbxVideo);
             this.Controls.Add(this.ibxOutput);
             this.Controls.Add(this.ibxSource);
@@ -180,8 +244,13 @@
         private Emgu.CV.UI.ImageBox ibxSource;
         private Emgu.CV.UI.ImageBox ibxOutput;
         private System.Windows.Forms.CheckBox cbxVideo;
-        private System.Windows.Forms.TextBox txtFrameLimit;
-        private System.Windows.Forms.Button btnSetFrameLimit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnWiimoteCalibrate;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.CheckBox cbxFlipHorizontal;
+        private System.Windows.Forms.Button btnCameraCalibrate;
+        private System.Windows.Forms.Label lblFPS;
     }
 }
 
