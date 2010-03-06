16062eccfd0a411808c37af8ae79613b48a858e5
aishwar <aishwar@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-03-06 19:18:06 +0000

    Contains common structures

---
+++ branches/WiimoteIRTracking/IdeaTester/Support.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IdeaTester
+{
+    public enum BandColor { White, Red, Green, Orange, Blue };
+}
