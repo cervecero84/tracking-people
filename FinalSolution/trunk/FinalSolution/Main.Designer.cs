bd7218c509e1d858ffd3b6d2668f07a758d95c82
aishwar <aishwar@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-03-07 21:55:52 +0000

    Project base

---
+++ FinalSolution/trunk/FinalSolution/Main.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace FinalSolution
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ibxOutput = new Emgu.CV.UI.ImageBox();
+            this.ibxSource = new Emgu.CV.UI.ImageBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ibxOutput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibxSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ibxOutput
+            // 
+            this.ibxOutput.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ibxOutput.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ibxOutput.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.ibxOutput.Location = new System.Drawing.Point(465, 28);
+            this.ibxOutput.Name = "ibxOutput";
+            this.ibxOutput.Size = new System.Drawing.Size(400, 300);
+            this.ibxOutput.TabIndex = 11;
+            this.ibxOutput.TabStop = false;
+            // 
+            // ibxSource
+            // 
+            this.ibxSource.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ibxSource.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ibxSource.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.ibxSource.Location = new System.Drawing.Point(29, 28);
+            this.ibxSource.Name = "ibxSource";
+            this.ibxSource.Size = new System.Drawing.Size(400, 300);
+            this.ibxSource.TabIndex = 10;
+            this.ibxSource.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(462, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Wiimote";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Camera";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 394);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ibxOutput);
+            this.Controls.Add(this.ibxSource);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ibxOutput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibxSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox ibxOutput;
+        private Emgu.CV.UI.ImageBox ibxSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FinalSolution/trunk/FinalSolution/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TableTopCommunicator;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using WiimoteLib;
+
+namespace FinalSolution
+{
+    public partial class Main : Form
+    {
+        Communicator comm = new Communicator();
+        Capture camera;
+        Wiimote wiimote;
+
+        public Main()
+        {
+            InitializeComponent();
+            comm.TouchReceived += new Communicator.TouchReceivedHandler(comm_TouchReceived);
+        }
+
+        private void comm_TouchReceived(object sender, TouchEventArgs t)
+        {
+            TouchInfo currTouch = t.Touch;
+        }
+    }
+}
