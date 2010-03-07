9b19506e696fd4cb1216b52d6c670a3f70c5ddc4
aishwar <aishwar@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-03-07 05:48:30 +0000

    New location in SVN for communicator project. [Organizing SVN in a way that makes sense.]

---
+++ TableTopCommunicator/trunk/TableTopCommunicator/TouchInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TableTopCommunicator
+{
+    [Serializable]
+    public struct TouchInfo
+    {
+        public int X;
+        public int Y;
+        public bool Exists;
+        public string UID;
+        public int timeIn;
+        public int timeOut;
+        public int orientation;
+        public int handPlacement;
+    }
+}
