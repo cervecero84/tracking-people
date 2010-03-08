0ae187537e3ae470815bd25b63d58c3c00ed98b5
aishwar <aishwar@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-03-08 03:58:25 +0000

    Structure that contains the set of calibration points

---
+++ FinalSolution/trunk/FinalSolution/CalibrationPoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace FinalSolution
+{
+    class CalibrationPoints
+    {
+        public PointF TL { get; set; }
+        public PointF TR { get; set; }
+        public PointF BL { get; set; }
+        public PointF BR { get; set; }
+    }
+}
