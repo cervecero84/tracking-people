12c3bf98679f7bb84804cb3f393f99e3175adcd3
aishwar <aishwar@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-03-08 03:58:47 +0000

    Structure that contains the band colors

---
+++ FinalSolution/trunk/FinalSolution/BandColor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalSolution
+{
+    enum BandColor
+    {
+        Red,
+        Green,
+        Yellow,
+        Blue
+    }
+}
