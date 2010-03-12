70c7388885ccb379b341a8842ae8d39a4c59d3e1
mimi.xia <mimi.xia@208d1520-c027-11de-8cd0-f941f5a20a08>
2010-03-12 09:52:59 +0000

    added HandProb code which invludes function that will return probability of two touch being from same hand. also needed to add a NormalDistribution class to assist with PDF calculation.

---
+++ FinalDesign/trunk/FinalDesign/HandProb.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using WiimoteLib;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using CenterSpace.Free;
+using Emgu.CV.CvEnum;
+using Emgu.CV.UI;
+
+namespace FinalSolution
+{
+    class HandProb
+    {
+        #region Static Variables : Anthropometic data on human hans ----------------------------------------
+        // data are taken from http://en.wikipedia.org/wiki/File:HandAnthropometry.JPG
+        // units are cm
+        private static double _mean = 18.65;
+        private static double _var = 0.85;
+        private static double handWidth = 8.45;
+
+        private static NormalDist handPDF = new NormalDist(_mean, _var);
+
+        #endregion Static Variable
+
+
+        public static double SkinConnectedProb(Image<Bgr, Byte> region, WiimoteLib.PointF touch, WiimoteLib.PointF ir, float pxRatio)
+        {
+            double sizeProb = HandSizeProb(touch, ir, pxRatio);
+            Image<Bgr, Byte> handROI=region.Clone();
+            Rectangle rectROI=Utility.getROI(touch, ir, handWidth);
+            CvInvoke.cvSetImageROI(handROI, rectROI);
+            Image<Gray, byte> skinPixels = SkinDetect(handROI);
+            double skinProb = skinPixels.GetAverage().Intensity / (skinPixels.Rows * skinPixels.Cols * 0.75); // 0.75 is a fudge factor, since hand will most like occupy a whole rectangle of ROI region
+
+            return skinProb * sizeProb;
+        }
+
+        public static double HandSizeProb(WiimoteLib.PointF touch, WiimoteLib.PointF ir, float pxRatio)
+        {
+            double dist = Math.Sqrt(Math.Pow(((touch.X - ir.X) * pxRatio),2) + Math.Pow(((touch.Y - ir.Y) * pxRatio),2));
+            double ProbDist=handPDF.PDF(dist);
+            return ProbDist;
+        }
+
+        public static Image<Gray, byte> SkinDetect(Image<Bgr, byte> Img)
+        {
+
+
+            Image<Gray, byte> S = new Image<Gray, byte>(Img.Width, Img.Height);
+            Image<Gray, byte> skin = new Image<Gray, byte>(Img.Width, Img.Height);
+
+            /* convert RGB color space to IRgBy color space using this formula:
+            http://www.cs.hmc.edu/~fleck/naked-skin.html
+            I = L(G)
+            Rg = L(R) - L(G)
+            By = L(B) - [L(G) +L(R)] / 2
+            					
+            to calculate the hue:
+            hue = atan2(Rg,By) * (180 / 3.141592654f)
+            Saturation = sqrt(Rg^2 + By^2)
+            */
+
+            for (int j = skin.Width - 1; j >= 0; j--)
+            {
+                for (int i = skin.Height - 1; i >= 0; i--)
+                {
+
+                    int R = Img.Data[i, j, 2];
+                    int G = Img.Data[i, j, 1];
+                    int B = Img.Data[i, j, 0];
+
+                    double Rg = Math.Log(R) - Math.Log(G);
+                    double By = Math.Log(B) - (Math.Log(G) + Math.Log(R)) / 2;
+
+                    double hue_val = Math.Atan2(Rg, By) * (180 / Math.PI);
+                    double sat_val = Math.Sqrt(Rg * Rg + By * By) * 255;
+
+
+                    if (sat_val >= 20 && sat_val <= 130 && hue_val >= 110 && hue_val <= 180) //I simplified the naked people filter's two overlapping criteria
+                    {
+                        S[i, j] = new Gray(255);
+                    }
+                    else
+                    {
+                        S[i, j] = new Gray(0);
+                    }
+                }
+            }
+
+
+            //skin = S.Erode(1);
+            skin = S.SmoothMedian(15); // median filter is used so that the image will be kept black and white
+
+            return skin;
+
+        }
+    }
+}
